f63e330d386bffe1792e7bdb969b81f3dd1ad56c
Darshan-1012 <bdarshan524@gmail.com>
2025-03-15 16:50:25 +0530

    till elif

--- New/main.go
+++ New/main.go
@@ -1,10 +1,25 @@
 package new
 
-import "fmt"
+const EnglishPrefix = "Hello "
+const SpainPrefix = "Hola "
+const FrenchPrefix = "Bonjour "
+const Spain = "Spain"
+const French = "French"
 
-func hello(name string) string {
-	return "Hello " + name
+func hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+	if language == Spain {
+		return SpainPrefix + name
+	}
+	if language == French {
+		return FrenchPrefix + name
+	}
+	return EnglishPrefix + name
 }
-func main() {
-	fmt.Println(hello("Darshan"))
-}
+
+/*The main is first step*/
+// func main() {
+// 	fmt.Println(hello("Darshan"))
+// }
--- New/main_test.go
+++ New/main_test.go
@@ -5,10 +5,34 @@ import (
 )
 
 func TestMain(t *testing.T) {
-	got := hello("Darshan")
-	want := "Hello Darshan"
+	// t.Run("The 'Hello Darshan' is executed", func(t *testing.T) {
+	// 	got := hello("Darshan", "")
+	// 	want := "Hello Darshan"
+	// 	assertMsg(t, got, want)
+	// })
 
+	t.Run("The 'Hello World' is executed", func(t *testing.T) {
+		got := hello("", "")
+		want := "Hello World"
+		assertMsg(t, got, want)
+	})
+
+	t.Run("hola in spain", func(t *testing.T) {
+		got := hello("Darshan", "Spain")
+		want := "Hola Darshan"
+		assertMsg(t, got, want)
+	})
+
+	t.Run("bonjour in French", func(t *testing.T) {
+		got := hello("Darshan", "French")
+		want := "Bonjour Darshan"
+		assertMsg(t, got, want)
+	})
+}
+
+func assertMsg(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
-		t.Errorf("%q is got \n %q is want ", got, want)
+		t.Errorf("got is %q and want is %q", got, want)
 	}
 }
