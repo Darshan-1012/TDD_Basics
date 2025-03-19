038b2b9e3f798a62efb310fd60169e510ccf3c75
Darshan-1012 <bdarshan524@gmail.com>
2025-03-19 19:09:13 +0530

    The array and slices part which is a bit complex

--- arrndslices/sum.go
+++ arrndslices/sum.go
@@ -7,3 +7,27 @@ func sum(numbers []int) int {
 	}
 	return sum
 }
+
+func SumAll(numberToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numberToSum {
+		sums = append(sums, sum(numbers))
+	}
+	return sums
+}
+
+func SumAllTrails(numberToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numberToSum {
+
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tails := numbers[1:]
+			sums = append(sums, sum(tails))
+		}
+	}
+	return sums
+}
--- arrndslices/sum_test.go
+++ arrndslices/sum_test.go
@@ -1,6 +1,9 @@
 package arrndslices
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("The test to sum = 5", func(t *testing.T) {
@@ -10,5 +13,36 @@ func TestSum(t *testing.T) {
 		if got != want {
 			t.Errorf("%q is got and %q is want for %v", got, want, numbers)
 		}
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	t.Run("The sum []int{1,2,3} = []int{6}", func(t *testing.T) {
+		got := SumAll([]int{3, 0}, []int{3, 9})
+		want := []int{3, 12}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("%v is got and %v is want", got, want)
+		}
+	})
+}
+func TestSumAllTrails(t *testing.T) {
+
+	checksums := func(t testing.TB, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("%v is got and %v is want ", got, want)
+		}
+	}
+	t.Run("The sum  of trials", func(t *testing.T) {
+		got := SumAllTrails([]int{2, 3}, []int{0, 9})
+		want := []int{3, 9}
+		checksums(t, got, want)
+	})
+
+	t.Run("The sum  of empty trials", func(t *testing.T) {
+		got := SumAllTrails([]int{}, []int{1, 9})
+		want := []int{0, 9}
+		checksums(t, got, want)
 	})
 }
