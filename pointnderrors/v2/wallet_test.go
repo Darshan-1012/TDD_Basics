2fa01dbd8333f153b786e08a356631d0bdc0e83d
Darshan-1012 <bdarshan524@gmail.com>
2025-03-20 18:57:55 +0530

    incufficient Balance and refactoring

--- pointnderrors/v2/wallet_test.go
+++ pointnderrors/v2/wallet_test.go
@@ -1,1 +1,47 @@
 package pointnderrors
+
+import "testing"
+
+func TestWallet(t *testing.T) {
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{Bitcoin(20)}
+		wallet.Withdraw(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("Insufficient funds", func(t *testing.T) {
+		startingBalance := Bitcoin(20)
+		wallet := Wallet{startingBalance}
+		err := wallet.Withdraw(Bitcoin(100))
+
+		assertError(t, err, "cannot withdraw, insufficient funds")
+		assertBalance(t, wallet, startingBalance)
+	})
+
+}
+
+func assertBalance(t testing.TB, wallet Wallet, want Bitcoin) {
+	t.Helper()
+	got := wallet.Balance()
+
+	if got != want {
+		t.Errorf("%s is got %s is want", got, want)
+	}
+}
+
+func assertError(t testing.TB, got error, want string) {
+	t.Helper()
+	if got == nil {
+		t.Error("Wanted an error but didn't get one")
+	}
+
+	if got.Error() != want {
+		t.Errorf("%q is got %q is want", got, want)
+	}
+}
