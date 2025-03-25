93f20e677133b8611b233c0e4accac6def89edeb
Darshan-1012 <bdarshan524@gmail.com>
2025-03-25 17:12:08 +0530

    Corrected the concurrency using channel

--- concurrency/v1/check_websites.go
+++ concurrency/v1/check_websites.go
@@ -9,6 +9,7 @@ type result struct {
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
 	resultChannel := make(chan result)
+
 	for _, url := range urls {
 		go func() {
 			resultChannel <- result{url, wc(url)}
@@ -18,7 +19,5 @@ func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 		r := <-resultChannel
 		results[r.string] = r.bool
 	}
-
-	// time.Sleep(2 * time.Second)
 	return results
 }
